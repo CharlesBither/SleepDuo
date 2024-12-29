2576cedab55184a655a3c87720cbc495f1c9ec6b
Charles Bither <95773999+CharlesBither@users.noreply.github.com>
2024-12-29 11:31:22 -0800

    add react-native-health-connect dependency

---
+++ app/components/SleepOverview.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, type TextProps, StyleSheet } from 'react-native';
+
+type Props = {
+    sleepData?: String, 
+};
+
+export default function SleepOverview({ sleepData }: Props) {
+    if (sleepData) {
+        return (
+            <>
+                <Text style={styles.text}>Sleep Overview</Text>
+                <Text style={styles.text}>{sleepData}</Text>
+            </>
+        );
+    }
+    return (
+        <>
+            <Text style={styles.text}>Sleep Overview</Text>
+        </>
+        
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#25292e',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text: {
+      color: '#fff',
+    },
+  });
